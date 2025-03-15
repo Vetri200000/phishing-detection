e0fecc96fdab72fd0bd7b417a8de44ecd16c1db0
Vetri200000 <161691132+Vetri200000@users.noreply.github.com>
2025-03-15 13:44:31 +0530

    Delete vetri phishing_Detection/phishing-detection-c directory

--- vetri phishing_Detection/phishing-detection-c/sample login git/script.js
+++
@@ -1,8 +0,0 @@
-function loginWithGoogle() {
-    const clientId = "623565397438-56j4lk55qso3vcuosq10rpb4cosrv98n.apps.googleusercontent.com";
-    const redirectUri = "http://localhost:8000/auth/callback"; // Your backend URL
-    const scope = "email profile";
-    const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
-    
-    window.location.href = authUrl;
-}
